2129a7925c5608697f9e8f88878f4794bd13faae
mells012 <mellstephania@gmail.com>
2024-11-16 17:58:52 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebListaC2.Models;
+
+namespace WebListaC2.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public List<Person> persons = new List<Person> 
+        { 
+            new Person { PersonId = 1, FirstName = "John", LastName = "Doe", Age = 25 }, 
+            new Person { PersonId = 2, FirstName = "Juan", LastName = "Perez", Age = 22 }, 
+            new Person { PersonId = 3, FirstName = "Tom", LastName = "Smith", Age = 30 } 
+        };
+
+        public IActionResult Index() 
+        { 
+            return View(persons); 
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace WebListaC2.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public byte Age { get; set; }
+    }
+
+}
