18d112c07ed561fc6acf5670f9c0e8f037ac68fb
mells012 <mellstephania@gmail.com>
2024-11-16 18:01:13 -0500

    Model Person creado y Depura

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -5,16 +5,16 @@ namespace WebListaC2.Controllers
 {
     public class PersonsController : Controller
     {
-        public List<Person> persons = new List<Person> 
-        { 
-            new Person { PersonId = 1, FirstName = "John", LastName = "Doe", Age = 25 }, 
-            new Person { PersonId = 2, FirstName = "Juan", LastName = "Perez", Age = 22 }, 
-            new Person { PersonId = 3, FirstName = "Tom", LastName = "Smith", Age = 30 } 
+        public List<Person> persons = new List<Person>
+        {
+            new Person { PersonId = 1, FirstName = "John", LastName = "Doe", Age = 25 },
+            new Person { PersonId = 2, FirstName = "Juan", LastName = "Perez", Age = 22 },
+            new Person { PersonId = 3, FirstName = "Tom", LastName = "Smith", Age = 30 }
         };
 
-        public IActionResult Index() 
-        { 
-            return View(persons); 
+        public IActionResult Index()
+        {
+            return View(persons);
         }
     }
 }
